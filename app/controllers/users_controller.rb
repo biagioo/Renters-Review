f29b75560e5dd5cd40e345ee812dcd53e9003438
biagioo <mendolia2328@gmail.com>
2020-05-04 16:15:47 -0400

    working on logic for creating a new tenant and landlord

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,53 @@
 class UsersController < ApplicationController
 
     def new
-        @user = User.new
-    end
-
-    def create
-        byebug
-    end
-
-    def edit
-    end
-
-    def update
         
     end
 
+    def create
+        if params[:landlord] == "0"
+            user = Tenant.new.build_user(user_params)
+            if user.valid?
+                user.save
+                t = Tenant.find_by(id: user[:userable_id])
+                t.first_name = userable_params[:first_name]
+                t.last_name = userable_params[:last_name]
+                t.age = userable_params[:age]
+                t.about = userable_params[:about]
+                t.save
+            else
+                redirect :new, alert:
+            end
+        else
+            user = Landlord.new.build_user(user_params)
+            if user.valid?
+                user.save
+                l = Landlord.find_by(id: user[:userable_id])
+                l.first_name = userable_params[:first_name]
+                l.last_name = userable_params[:last_name]
+                l.age = userable_params[:age]
+                l.about = userable_params[:about]
+                l.save
+            else
+                redirect :new, alert: user.error.full_messages
+            end
+        end
+    end
+
+    def show
+    end
+
     def delete
+    end
+
+    private
+
+    def user_params
+        params.permit(:email, :password)
+    end
+
+    def userable_params
+        params.permit(:first_name, :last_name, :age, :about)
     end
     
 end
